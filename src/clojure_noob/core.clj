3f033b9a990d3970c882d97a315fdf3b3e61967b
Micah Resnick <resnickmicah@gmail.com>
2025-04-05 18:49:14 -0400

    Finish part of ch. 3
    Skipped ch2 because I'm not using Emacs
    Up next is `when`:
    The when operator is like a combination of if and do, but with no else branch. Here’s an example:

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,7 +1,27 @@
 (ns clojure-noob.core
   (:gen-class))
 
+;; Ch. 1: Building, running, and the REPL
+(defn teapot
+  []
+  (println "I'm a little teapot!"))
+
+(defn ch2
+  []
+  (do (+ 1 2 3)
+      (if true
+        "By Zeus's Hammer!"
+        "By Aquaman's Trident!")
+      (if false
+        "BZH!"
+        "BAT!")
+      (if true
+        (do (println "Success!")
+            "BZH!")
+        (do (println "Failure!")
+            "BAT!"))))
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "I'm a little teapot!"))
+  (do (teapot)
+      (println (ch2))))
